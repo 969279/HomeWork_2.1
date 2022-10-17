60165147d51b618095466343fa90093502e510ba
Alexandr <guk.zverolis@yandex.ru>
2022-10-17 19:04:48 +0300

    корректировка задания

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -3,27 +3,16 @@
 int num1 = Convert.ToInt32(Console.ReadLine());
 int num2 = Convert.ToInt32(Console.ReadLine());
 int num3 = Convert.ToInt32(Console.ReadLine());
-int max1 = 0, max2 = 0;
+int max = num1;
 
 if (num1 == num2 && num2 ==num3)
 {
      Console.WriteLine("No max");
 }
 else {
-if (num1 > num2)
+if (num2 > max) {max = num2;}
+if (num3 > max) {max = num3;}
     {
-    max1 = num1;
+    Console.WriteLine("max = " + max);
     }
-else max1 = num2;
-if (num2 > num3)
-    {
-    max2 = num2;
-    }
-else max2 = num3;
-if (max1 > max2)
-    {
-    Console.WriteLine("max = " + max1);
-    }
-else 
-    Console.WriteLine("max = " + max2);
 }
